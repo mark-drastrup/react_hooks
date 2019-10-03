553aef96caaeff6ec138b466587d075fb4356215
mark-drastrup <mark.drastrup@gmail.com>
2019-10-03 23:32:30 +0900

    Adding simple 'useEffect' to todolist

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Todo from "./Todo";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    document.title = `You have ${todos.length} todos`;
+  });
 
   const handleUpdate = e => {
     setInputValue(e.target.value);
@@ -18,6 +22,7 @@ const TodoList = () => {
   return (
     <div>
       <h1>Todo List</h1>
+      <p>You have </p>
       <ul>
         {todos &&
           todos.map((todo, index) => (
